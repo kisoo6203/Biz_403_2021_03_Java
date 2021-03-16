4801fb174aadaf3a9341a0bf8c08f0003589c740
kisoo6203 <kisoo6203@naver.com>
2021-03-16 16:42:06 +0900

    연산

---
+++ Java_00_Hello/src/com/callor/hello/Nums02.java
@@ -0,0 +1,20 @@
+package com.callor.hello;
+
+public class Nums02 {
+
+	public static void main(String[] args) {
+	
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+		System.out.println(1  % 2);
+		System.out.println(2  % 2);
+		System.out.println(3  % 2);
+		System.out.println(4  % 2);
+		System.out.println(5  % 2);
+		
+	}
+	
+}
