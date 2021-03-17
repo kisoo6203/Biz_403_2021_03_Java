0458939b11e998438a2be2fd941e4fcb6a1abc59
kisoo6203 <kisoo6203@naver.com>
2021-03-17 10:04:07 +0900

    연산복습

--- Java_00_Hello/src/com/callor/hello/Numbers.java
+++ Java_00_Hello/src/com/callor/hello/Numbers.java
@@ -4,11 +4,13 @@ public class Numbers {
 	
 	public static void main(String[] args) {
 
-	System.out.println("33 + 2 = 35");
+	System.out.println("33 + 2 = " + (33+2));
 	System.out.println("33 - 2 = " + (33-2));
 	System.out.println("33 * 2 = " + 33*2);
 	System.out.println("33 / 2 = " + 33 / 2 );
 	System.out.println("33 % 2 = " + 33 % 2);
 
+	
+	
 	}
 }
