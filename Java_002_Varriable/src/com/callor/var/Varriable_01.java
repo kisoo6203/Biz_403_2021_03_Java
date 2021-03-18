eea4e486d67766da49442d3d37a658f1b9014063
kisoo6203 <kisoo6203@naver.com>
2021-03-18 09:24:05 +0900

    아침 복습

---
+++ Java_002_Varriable/src/com/callor/var/Varriable_01.java
@@ -0,0 +1,24 @@
+package com.callor.var;
+
+public class Varriable_01 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 33;
+		int num2 = 55;
+		
+		int sum = num1 + num2;
+		int minus = num2 - num1;
+		int times = num1 * num2;
+		int div = num2 / num1;
+		
+		System.out.printf("%d + %d = %d\n", num1, num2, sum  );
+		System.out.printf("%d - %d = %d\n", num2, num1, minus  );
+		System.out.printf("%d * %d = %d\n", num1, num2, times  );
+		System.out.printf("%d / %d = %d\n", num2, num1, div  );
+		
+		
+	}
+	
+	
+}
